3e4f2b5b89f7115195826fe422e8174234297453
Dacharat Pankong <dacharat.p@ku.th>
2017-04-03 15:37:53 +0700

    Edit something

--- Rec.java
+++ Rec.java
@@ -8,8 +8,7 @@ public class Rec {
 	public static int numDigits(int n) {
 		if (n == 0)
 			return 1;
-		if (n >= 0 && n < 10)
-			return 1;
+		if (n >= 0 && n < 10) return 1;
 		else
 			return 1 + numDigits(n / 10);
 		// fill me in
